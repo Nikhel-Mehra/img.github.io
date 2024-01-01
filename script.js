1cb3edce979359cddd64e2787e866ad3bd08bec4
Nikhelmehra <149101254+Nikhel-Mehra@users.noreply.github.com>
2024-01-02 01:57:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
-function getGPS() {
-    // Make an AJAX request to the PHP script
+document.addEventListener("DOMContentLoaded", function() {
+    getLocation();
+});
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(savePosition, showError);
+    } else {
+        document.getElementById("coordinates").innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function savePosition(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    document.getElementById("coordinates").innerHTML = "Latitude: " + latitude + "<br>Longitude: " + longitude;
+
+    // Save coordinates to a text file using AJAX
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            // Display the retrieved coordinates
-            document.getElementById("coordinates").innerHTML = "GPS Coordinates: " + xhr.responseText;
+            console.log("Coordinates saved successfully.");
         }
     };
-    xhr.open("GET", "get_coordinates.php", true);
-    xhr.send();
+    xhr.open("POST", "save_coordinates.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("latitude=" + latitude + "&longitude=" + longitude);
+}
+
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            document.getElementById("coordinates").innerHTML = "User denied the request for Geolocation.";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            document.getElementById("coordinates").innerHTML = "Location information is unavailable.";
+            break;
+        case error.TIMEOUT:
+            document.getElementById("coordinates").innerHTML = "The request to get user location timed out.";
+            break;
+        case error.UNKNOWN_ERROR:
+            document.getElementById("coordinates").innerHTML = "An unknown error occurred.";
+            break;
+    }
 }
